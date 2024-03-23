8852fdcaf8647bd8b22055f9eee2da7eb4009d6f
mohammed hammami <mohammed.hammami@hotmail.com>
2024-03-23 13:29:55 -0900

    test 1

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require('fs');
+const https = require('https');
 require('dotenv').config();
-require('./models/db')
+require('./models/db');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 443; // 443 هو المنفذ الافتراضي لـ HTTPS
+
 const usersRouter = require("./routes/users");
-const infoRouter=require('./routes/info')
+const infoRouter = require('./routes/info');
+
 app.use(cors());
 app.use(express.json());
 
-
-
-
 app.use("/users", usersRouter);
-app.use("/info",infoRouter)
+app.use("/info",infoRouter);
+
 // Handles any other endpoints [unassigned - endpoints]
 app.use("*", (req, res) => res.status(404).json("NO content at this path"));
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
-});
+// استخدم مسار الملفات الخاصة بك هنا
+const httpsOptions = {
+  key: fs.readFileSync('/root/key.pem'), // تحديث المسار إلى مكان المفتاح الخاص
+  cert: fs.readFileSync('/root/cert.pem') // تحديث المسار إلى مكان شهادة SSL
+};
 
+https.createServer(httpsOptions, app).listen(PORT, () => {
+  console.log(`Server listening at https://localhost:${PORT}`);
+});
